6598aff78c1849abc6d0ce102e745e70346415d8
Haochii <hawkliu.game@gmail.com>
2020-12-21 00:11:50 +0800

    Update TimerAlert

---
+++ Project/Assets/Script/TimerAlert.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerAlert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((int)float.Parse(GetComponent<Text>().text) <= 3)
+        {
+            GetComponent<Text>().fontSize = 200;
+            GetComponent<Text>().color = Color.red;
+        }
+        else
+        {
+            GetComponent<Text>().fontSize = 160;
+            GetComponent<Text>().color = Color.white;
+        }
+    }
+}
