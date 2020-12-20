f9584e2fb11cff26e576045b4c0375c3bd19d8f3
Haochii <hawkliu.game@gmail.com>
2020-12-20 23:54:23 +0800

    Update RoundInfo.cs

--- Project/Assets/Script/RoundInfo.cs
+++ Project/Assets/Script/RoundInfo.cs
@@ -8,20 +8,29 @@ public class RoundInfo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        isShowing = true;
     }
     public float timer = 2.0f; // 定时2秒
-    private bool isShowing = false;
+    public bool isShowing;
     public void ShowRoundInfo()
     {
         isShowing = true;
+        GetComponent<Image>().enabled = true;
     }
 
     void Update()
     {
+        
         if(isShowing)
         {
+            bool isFading = false;
+            Debug.Log(timer.ToString());
             timer -= Time.deltaTime;
+            if (timer <=0.5 && !isFading)
+            {
+                GetComponent<Image>().CrossFadeAlpha(0, 0.3f, true);
+                isFading = true;
+            }
             if (timer <= 0)
             {
                 isShowing = false;
