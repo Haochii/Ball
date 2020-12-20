6a73439d5b49e764c3cadb2651aa4dd7ed1abc79
Haochii <hawkliu.game@gmail.com>
2020-12-20 23:17:44 +0800

    update rec pngs; Add roundinfo.cs

---
+++ Project/Assets/Script/RoundInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float timer = 2.0f; // 定时2秒
+    private bool isShowing = false;
+    public void ShowRoundInfo()
+    {
+        isShowing = true;
+    }
+
+    void Update()
+    {
+        if(isShowing)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                isShowing = false;
+                GetComponent<Image>().enabled = false;
+                timer = 2.0f;
+            }
+        }
+    }
+
+}
